3dfad9061b3fefcc7658f5dea9c8d01d789cc850
sergeykoryagin <seregakid1990@gmail.com>
2023-04-20 00:57:53 +0300

    add style reset

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from 'app/App';
+import './app/styles/index.css';
 
 const root = document.getElementById('root');
 
